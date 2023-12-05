2125e84ed145ee4e5124d91ff4aec95fb9fbc4a1
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-12-05 15:13:35 -0300

    commit 3

--- src/components/nova-transacao-component.ts
+++ src/components/nova-transacao-component.ts
@@ -1,3 +1,8 @@
+import Conta from "../types/Conta.js";
+import { TipoTransacao } from "../types/TipoTransacao.js";
+import { Transacao } from "../types/Transacao.js";
+import SaldoComponent from "./saldo-component.js";
+
 const elementoFormulario = document.querySelector(".block-nova-transacao form") as HTMLFormElement;
 elementoFormulario.addEventListener("submit", function(event) {
   event.preventDefault();
@@ -14,25 +19,14 @@ elementoFormulario.addEventListener("submit", function(event) {
   let tipoTransacao: TipoTransacao = inputTipoTransacao.value as TipoTransacao;
   let valor: number = inputValor.valueAsNumber;
   let data: Date = new Date(inputData.value);
-
-  if (tipoTransacao === TipoTransacao.DEPOSITO) {
-    saldo += valor;
-  } else if (tipoTransacao === TipoTransacao.TRANSFERENCIA || tipoTransacao === TipoTransacao.PAGAMENTO_BOLETO) {
-    saldo -= valor;
-  } else {
-    alert("Tipo de Transação é inválido!");
-    return;
-  }
-
-  elementoSaldo.textContent = formatarMoeda(saldo);
-
+  
   const novaTransacao: Transacao = {
     tipoTransacao: tipoTransacao,
     valor: valor,
     data: data
   };
-
-  console.log(novaTransacao);
-
+  
+  Conta.registrarTransacao(novaTransacao);
+  SaldoComponent.atualizar();
   elementoFormulario.reset();
 });
--- src/components/saldo-component.ts
+++ src/components/saldo-component.ts
@@ -1,12 +1,24 @@
-let saldo: number = 3000;
+import Conta from "../types/Conta.js";
+import { formatarData, formatarMoeda } from "../utils/formatters.js";
+
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
 const elementoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement;
 
-if (elementoSaldo !== null) {
-  elementoSaldo.textContent = formatarMoeda(saldo);
+if (elementoDataAcesso !== null) {
+  elementoDataAcesso.textContent = formatarData(Conta.getDataAcesso(), FormatoData.DIA_SEMANA_DIA_MES_ANO);
 }
 
-if (elementoDataAcesso !== null) {
-  const dataAcesso: Date = new Date();
-  elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO);
+renderizarSaldo();
+function renderizarSaldo(): void {
+  if (elementoSaldo !== null) {
+    elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
+  }
 }
+
+const SaldoComponent = {
+  atualizar() {
+    renderizarSaldo();
+  }
+};
+
+export default SaldoComponent;
