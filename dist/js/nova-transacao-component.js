47384d5e14999ccf07578374df48c818e286f7a2
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-12-04 23:05:16 -0300

    commit 2

--- dist/js/nova-transacao-component.js
+++ dist/js/nova-transacao-component.js
@@ -21,7 +21,7 @@ elementoFormulario.addEventListener("submit", function (event) {
         alert("Tipo de Transação é inválido!");
         return;
     }
-    elementoSaldo.textContent = saldo.toString();
+    elementoSaldo.textContent = formatarMoeda(saldo);
     const novaTransacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
